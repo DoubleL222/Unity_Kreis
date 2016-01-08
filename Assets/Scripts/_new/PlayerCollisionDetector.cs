3ab2ae4bce1dbc7f76de9d77f2861d1eaaffa321
Luka Ločniškar <luka.locniskar@gmail.com>
2016-01-08 17:10:04 +0100

    playerCollisionDetection

---
+++ Assets/Scripts/_new/PlayerCollisionDetector.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCollisionDetector : MonoBehaviour {
+	Rigidbody2D rigidBody;
+	void Start(){
+		rigidBody = gameObject.GetComponent<Rigidbody2D> ();
+	}
+	// Use this for initialization
+	void OnCollisionEnter2D(Collision2D coll)
+	{
+
+
+		if (coll.gameObject.tag == "Player") {
+			Debug.Log (" coll. rigidbody.velocity" + coll.rigidbody.velocity);
+			//Debug.Log ("relative Velocity " + coll.relativeVelocity);
+			Rigidbody2D myRbd = coll.gameObject.GetComponent<Rigidbody2D>();
+			if(myRbd != null){
+				Vector2 myVelocity = myRbd.velocity;
+				Rigidbody2D hisRbd = coll.rigidbody;
+				if(hisRbd!=null){
+				Vector2 hisVelocity = hisRbd.velocity;
+					//Debug.Log("his velocity "+hisVelocity + " my velocity "+ myVelocity);
+					if(myVelocity.magnitude > hisVelocity.magnitude){
+						//Debug.Log("pushing him for" +coll.relativeVelocity);
+						hisRbd.AddForce(coll.relativeVelocity);
+					}
+				}
+			}
+		}
+	}
+}
