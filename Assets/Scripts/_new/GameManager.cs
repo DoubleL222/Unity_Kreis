6110088667b5d66290e0b750ee6966e5b4089428
Luka Ločniškar <luka.locniskar@gmail.com>
2016-01-14 23:26:29 +0100

    finished Effects implementation

--- Assets/Scripts/_new/GameManager.cs
+++ Assets/Scripts/_new/GameManager.cs
@@ -9,11 +9,18 @@ public class GameManager : MonoBehaviour {
 	GameObject localPlayer1;
 	GameObject localPlayer2;
 
+	GameObject PhaseInEffect;
 	// Use this for initialization
 	void Start() {
 		GameObject localPlayerPrefab = Resources.Load ("_new/LocalPlayer") as GameObject;
-		localPlayer1 = MonoBehaviour.Instantiate (localPlayerPrefab, new Vector3 (0, 17.5f, 0), new Quaternion ()) as GameObject;
-		localPlayer2 = MonoBehaviour.Instantiate (localPlayerPrefab, new Vector3 (0, 17.5f, 0), new Quaternion ()) as GameObject;
+		PhaseInEffect = Resources.Load ("_new/PhaseInEffect") as GameObject;
+
+		Vector3 SpawnPosition = new Vector3 (0, 15f, 0);
+
+		Instantiate (PhaseInEffect, transformToPolar (SpawnPosition), Quaternion.identity);
+
+		localPlayer1 = MonoBehaviour.Instantiate (localPlayerPrefab, SpawnPosition, new Quaternion ()) as GameObject;
+		localPlayer2 = MonoBehaviour.Instantiate (localPlayerPrefab, SpawnPosition, new Quaternion ()) as GameObject;
 
 		IDictionary<string,string> p2keys = new Dictionary<string,string> ();
 		p2keys.Add ("left", "g");
@@ -31,7 +38,16 @@ public class GameManager : MonoBehaviour {
 		//rings.Add (new RingManager (5f));
 		//Debug.Log ("Initialized!");
 	}
-	
+	Vector3 transformToPolar(Vector3 pos){
+		
+		float angle = pos.x / 10.0f;
+		float distance = pos.y;
+		
+		float mx = distance * Mathf.Cos (angle);
+		float my = distance * Mathf.Sin (angle);
+		
+		return new Vector3 (mx, my, 0.0f);
+	}
 	// Update is called once per frame
 	void Update () {
 	
