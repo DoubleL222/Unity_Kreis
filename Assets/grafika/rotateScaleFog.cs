459e3e72d53ffd0cbfb2b1bdd37314af0fe3774b
Juš Lozej <jlozej@gmail.com>
2016-01-15 21:05:53 +0100

    Assets rescaling

--- Assets/grafika/rotateScaleFog.cs
+++ Assets/grafika/rotateScaleFog.cs
@@ -5,11 +5,20 @@ public class rotateScaleFog : MonoBehaviour {
     public float rotationSpeed = 1;
     public float resizeSpeed = 0.2f;
     public float scaleChange = 0.1f;
+    public float sizePhase = 0.0f;
     Vector3 startingScale;
     private float currentRot=0;
     public Vector3 rotationVector;
+    public bool randomInit = false;
 	// Use this for initialization
 	void Start () {
+        if (randomInit)
+        {
+            rotationSpeed = Random.Range(-3,3);
+            resizeSpeed = Random.Range(-3, 3);
+            sizePhase = Random.Range(0, 120);
+
+        }
         startingScale = transform.localScale;
     }
 	
@@ -17,7 +26,7 @@ public class rotateScaleFog : MonoBehaviour {
 	void FixedUpdate () {
         transform.Rotate(rotationVector*rotationSpeed);
         currentRot = currentRot % 360;
-        transform.localScale = new Vector3(startingScale.x+(Mathf.Sin(currentRot*Mathf.Deg2Rad)*scaleChange), startingScale.y + (Mathf.Sin(currentRot * Mathf.Deg2Rad) * scaleChange), startingScale.z);
+        transform.localScale = new Vector3(startingScale.x+(Mathf.Sin((currentRot + sizePhase) *Mathf.Deg2Rad)*scaleChange), startingScale.y + (Mathf.Sin(currentRot * Mathf.Deg2Rad) * scaleChange), startingScale.z);
         currentRot += resizeSpeed;
 	}
 }
